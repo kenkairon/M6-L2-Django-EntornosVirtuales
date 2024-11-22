328e72e89e9b7adee7df897ffc768fa3cf55bb8b
kenkairon <ingeinfo2006@gmail.com>
2024-11-22 03:31:50 -0300

    Ejercicio Completo templates

--- leccion2/leccion2/settings.py
+++ leccion2/leccion2/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dia2'
+    'dia2',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'leccion2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR,'templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
